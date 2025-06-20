e72c736fbafe868d1268482f6556a7abe8d40325
CHunjat <minuszz@naver.com>
2025-06-20 17:49:43 +0900

    2025.6.20. 트리의 노드 삭제 구현 및 k번째 큰 수 찾기

--- CH_6 Tree/main.cpp
+++ CH_6 Tree/main.cpp
@@ -40,6 +40,22 @@
 * bst 이진 검색 알고리즘으로 자료구조를 표현한 것
 * 단점 : 트리가 한쪽으로 편향 될 경우 O(n) 시간만큼 걸린다.
 */
+
+/*
+* 트리 자료구조의 용어
+* 노드 : 3가지 데이터 저장 : (값(VALUE), 왼쪽, 오른쪽 노드 주소) 트리의 기본 요소를 저장하는 데이터 단위
+* 부모 노드 : 하위의 노드와 직접 연결된 노드
+* 자식 노드 : 부모 노드의 하위에 직접 연결된 노드
+* 루트 노드 : 트리의 최상위에 있는 노드. 부모 노드가 없음
+* 조상 노드 : 특정 노드에서 루트까지의 경로에 있는 모든 노드
+* 후손 노드 : 특정 노드의 모든 자식들을 포함한 노드들 
+* 
+* - 알고리즘 트리 코드 구현 시 사용되는 용어 - 
+*	레벨(level) : 루트로 부터의 거리를 뜻합니다. 루트는 0을 반환합니다.
+*   높이        : 트리에서 가장 깊은 레벨을 뜻한다.
+*   깊이        : 특정 노드에서 루트까지의 길이입니다.
+*   서브 트리   : 트리 내부에 반복적으로 이루어지는 후손들로 구성된 또 다른 트리
+*/
 struct Node
 {
 	int value;
@@ -144,6 +160,73 @@ private:
 		}
 		return node;
 	}
+
+	Node* findMin(Node* node)
+	{
+		if (node == nullptr || node->leftNode == nullptr)
+		{
+			return node;
+		}
+		return findMin(node->leftNode);
+	}
+
+	Node* fineMax(Node* node)
+	{
+		if (node == nullptr || node->rightNode == nullptr)
+		{
+			return node;
+		}
+		return fineMax(node->rightNode);
+	}
+
+	Node* deleteNode(Node* node, int target) //삭제, 데이터를 찾은 다음에 있으면 지운다
+	{
+		if (node == nullptr) { return nullptr; }
+
+		if (node->value > target) //왼쪽 노드로 이동
+		{
+			node->leftNode = deleteNode(node->leftNode, target);
+		}
+		else if(node->value < target) //오른쪽 노드로 이동
+		{
+			node->rightNode = deleteNode(node->rightNode, target);
+		}
+		else             // (node->value = target) <-지울 데이터를 찾은 경우
+		{
+			// 찾았다, 삭제해라.
+
+			/*if (node->rightNode == nullptr && node->leftNode == nullptr);
+			if (node->leftNode == nullptr);
+			if (node->rightNode == nullptr);*/ // 밑에거처럼 하나로 표현가능
+
+
+			if (node->leftNode == nullptr || node->rightNode == nullptr)
+			{
+				Node* temp = node->leftNode != nullptr ? node->leftNode : node->rightNode;
+
+				if (temp == nullptr)
+				{
+					temp = node;
+					node = nullptr;
+				}
+				else
+				{
+					node = temp;
+				}
+			}
+			else //자식이 둘인경우
+			{
+				//오른쪽에서 가장 작은 값을 찾겠다.
+				Node* temp = findMin(node->rightNode);
+				node->value = temp->value;
+				node->rightNode = deleteNode(node->rightNode, temp->value);
+			}
+		}
+		return node;
+	}
+
+
+
 	void InOrder(Node* node)
 	{
 		if (node == nullptr) { return; }
@@ -170,13 +253,18 @@ public:
 		InOrder(root);
 	}
 
+	void DeleteNode(int value)
+	{
+		root = deleteNode(root, value);
+	}
+
 };
 
 #pragma endregion
 
 int main()
 {
-	NodeConnect();
+	//NodeConnect();
 	BST bst;	
 	bst.insert(4);
 	bst.insert(2);
@@ -185,7 +273,11 @@ int main()
 	bst.InOrder();
 	bst.insert(9);
 	bst.insert(7);
+	bst.insert(1);
 	std::cout << "이진 검색 트리 데이터 입력 예시" << std::endl;
+	bst.InOrder();
+	std::cout << " \n 6 데이터 삭제 후 결과 " << std::endl;
+	bst.DeleteNode(6);
 	bst.InOrder();
 
 }
---
+++ CH_7 KthFind/main.cpp
@@ -0,0 +1,76 @@
+/*
+* 알고리즘 대표 문제
+*  - 수를 정렬하고 탐색하기
+*  - 단순한 방법. 데이터를 정렬시킨다.
+*  - k번째로 작은수? 왼쪽부터 하나씩 출력
+*  - k번째로 큰 수 ? 오른쪽부터 하나씩 출력
+*/
+
+// 응급환자 순서를 제공해드립니다 . vector<int>
+// 먼저 진료해야하는 환자가 1번부터 시작합니다
+
+//트리에 저장방식을 응용해서  가장 큰 수를 찾는 형태를 구현하면 어떨까?
+
+#include <iostream>
+#include <vector>
+#include <utility> //pair 사용
+#include <map> //insert {pair -> people d}
+#include <algorithm> //sort 함수
+
+void EmergencyExample()
+{
+	std::vector<int> people = { 10,5,7,25,4,27,9 };
+	std::vector<int> answer;
+	answer.assign(people.size(), 0);
+	//? 
+
+	//answer.push_back()//
+	std::map<int, int> sheets;
+
+	for (int i = 0; i < people.size(); i++)
+	{
+		sheets.insert({ i,people[i] });
+	}
+
+
+	// people 의 값을 정렬
+	std::sort(people.begin(), people.end());
+
+	//answer에 들어있는 가장 큰 수?
+	//int key = sheets.find(answer[answer.size() - 1])->first; //sheets. 각 숙자에 들어있는 인덱스
+	//sheets 에 들어있는 인덱스값을 반환해주세요
+	//answer[key] = 1;
+
+	/*
+	* Tree 노드 삭제 
+	* Tree 균형이 안잡힌 경우
+	* 아이디어 1. 트리에 데이터를 넣을 때 균형잡히게 넣으면 되는거 아닌가?
+	* 채워 넣은 다음에 서브트리들이 하나의 규칙을 갖도록 해보자.
+	* Heap : 쌓여있다. 서브트리의 부모노드가 가장 큰수여야 한다. 가장 위에 있는 노드가 가장 큰 수.
+	* 가장 위에 있는 노드가 가장 큰 수, maxheap
+	* 가장 위에있는 노드가 가장 작은 수 , minheap
+	* heapify() - 트리를 합순서로 재정렬하는 코드 함수
+	* heapSort() - 힙정렬
+	*/
+	//정렬 , 탐색 같이 사용
+
+	// 반복문사용
+
+	for (int i = 0; i < people.size(); i++) //7
+	{
+		int key = sheets.find(people[people.size() - 1-i])->first; 
+		answer[key] = i + 1;
+	}
+
+	std::cout << "[";
+	for (int i = 0; i < answer.size(); i++)
+	{
+	  std::cout<< answer[i] << " ";
+	}
+	std::cout << "]" << std::endl;
+}
+
+int main()
+{
+	EmergencyExample();
+}
